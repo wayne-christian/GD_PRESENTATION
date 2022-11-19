f4824ea74e635073a3d2ec342749b4872a5a4633
wayne-christian <32497162+wayne-christian@users.noreply.github.com>
2022-11-19 11:25:02 +0000

    added show and trips

--- js/main.js
+++ js/main.js
@@ -89,7 +89,7 @@
 
             pane: {
               center: ['50%', '85%'],
-              size: '140%',
+              size: '100%',
               startAngle: -90,
               endAngle: 90,
               background: {
